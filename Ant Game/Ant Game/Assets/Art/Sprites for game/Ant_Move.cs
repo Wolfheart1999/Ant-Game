5f46cf17c16f5cb19063d1292791d1b1be748594
SUSSEXDOWNS\463074 <bethsmith1999@yahoo.co.uk>
2017-12-12 14:20:01 +0000

    Started to Code Movement

--- Ant Game/Ant Game/Assets/Art/Sprites for game/Ant_Move.cs
+++ Ant Game/Ant Game/Assets/Art/Sprites for game/Ant_Move.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Ant_Move : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    public float moveX;
+
+    // Update is called once per frame
+    void Update() {
+        PlayerMove();
+    }
+    void PlayerMove() {
+        //CONTROlS
+        moveX = Input.GetAxis("Horizontal");
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        //PHYSICS
+}
+    void Jump();
+    //JUMPING CODE
+
 }
